2f7ca6aabef472dd12754a70483f4d082999c3f8
aldia_mechdream <futami16237@gmail.com>
2025-05-21 15:46:52 +0800

    feat: 填补了没有翻译的部分； 全部合并到aitalk

--- src/aitalk.ts
+++ src/aitalk.ts
@@ -1,7 +1,7 @@
 l([
   // 老牌的
-  ['结月缘', '結月ゆかり', 'jun da meng'],
-  ['绁星灯', '紲星あかり', 'si guo mei tan'],
+  ['结月缘', '結月ゆかり', 'jie yue yuan'],
+  ['绁星灯', '紲星あかり', 'xie xing deng'],
   ['弦卷真纪', '弦巻マキ', 'xian juan zhen ji'],
   ['琴叶茜', '琴葉茜', 'qin ye qian'],
   ['琴叶茜', '琴葉茜', 'qin ye xi'],
@@ -37,58 +37,116 @@ l([
   ['鸣花尊', '鳴花ミコト', 'ming hua zun'],
 
   // # ai voice junior
-
-  // 結城香
-  // 日ノ出賢
-  // 潮崎かずき
-  // 風見壮一
-  // 羽ノ華
+  ['结城香', '結城香', 'jie cheng xiang'],
+  ['日诺出贤', '日ノ出賢', 'ri nuo chu xian'],
+  ['潮崎和树', '潮崎かずき', 'chao qi he shu'],
+  ['风见壮一', '風見壮一', 'feng jian zhuang yi'],
+  ['羽之华', '羽ノ華', 'yu zhi hua'],
   // 結月ゆかり雫
   // 結月ゆかり凪?
   // 紲星あかり蕾
   // 紲星あかり萌?
   // 琴葉茜蕾・葵蕾
   ['足立零', '足立レイ', 'zu li ling'],
-  // タンゲコトエ
-  // 式狼縁・式大元
-  // RIA(リア)
-  // 来果
-  // 夜語トバリ
-  // 咲ちゃん(高遠咲)
+  ['丹下琴恵', 'タンゲコトエ', 'dan xia qin hui'],
+  ['式狼缘', '式狼縁', 'shi lang yuan'],
+  ['式大元', '式大元', 'shi da yuan'],
+  ['莉亚', 'リア', 'li ya'],
+  ['来果', '来果', 'lai guo'],
+  ['夜语飞羽', '夜語トバリ', 'ye yu fei yu'],
+  ['高远咲', '高遠咲', 'gao yuan xiao'],
   // GUMI
   ['纺乃世词音', '紡乃世詞音', 'fang nai shi ci yin'],
 
   // # cevio/ai talk
-  // さとうささら
-  // すずきつづみ
-  // タカハシ
-  // ONE
-  // IA
+  ['砂糖小飒', 'さとうささら', 'sha tang xiao sa'],
+  ['铃木小鼓', 'すずきつづみ', 'ling mu xiao gu'],
+  ['铃木缇奏', 'すずきつづみ', 'ling mu ti zou'],
+  ['高桥', 'タカハシ', 'gao qiao'],
+  ['菲酱', 'フィーちゃん', 'fei jiang'],
+  ['罗莎', 'ロサ', 'luo sha'],
+  ['悠妮酱', 'ユニちゃん', 'you ni jiang'],
+  // ['', 'ONE', ''],
+  // ['', 'IA', ''],
   ['小春六花', '小春六花', 'xiao chun liu hua'],
-  // フィーちゃん
-  // ロサ(ROSA)
   ['夏色花梨', '夏色花梨', 'xia se hua li'],
   ['花隈千冬', '花隈千冬', 'hua wei qian dong'],
   ['双叶凑音', '双葉湊音', 'shuang ye cou yin'],
-  // ユニちゃん
 
   // # CoeFont
 
-  // アリアル
-  // ミリアル
-  // アベルーニ
-  // # COEIROINK
+  ['阿莉娅尔', 'アリアル', 'a li ya er'],
+  ['雅莉娅尔', 'アリアル', 'ya li ya er'],
+  ['米莉娅尔', 'ミリアル', 'mi li ya er'],
+  ['阿贝鲁尼', 'アベルーニ', 'a bei lu ni'],
+  ['卡内尔', 'カネル', 'ka nei er'],
 
-  // つくよみちゃん
-  // ディアちゃん
-  // アルマちゃん
-  // リリンちゃん
-  // クロワちゃん
+  // # COEIROINK
+  ['月读酱', 'つくよみちゃん', 'yue du jiang'],
+  ['蒂亚酱', 'ディアちゃん', 'di ya jiang'],
+  ['阿鲁玛酱', 'アルマちゃん', 'a lu ma jiang'],
+  ['莉琳酱', 'リリンちゃん', 'li lin jiang'],
+  ['库洛瓦酱', 'クロワちゃん', 'ku luo wa jiang'],
+
   // # VOICEPEAK
 
   // 彩澄しゅお
-  // 彩澄りりせ
   // フリモメン
+  ['', '彩澄しゅお', ''],
+  ['彩澄莉莉世', '彩澄りりせ', 'cai cheng li li shi'],
+  ['', 'フリモメン', ''],
   ['邪神酱', '邪神ちゃん', 'xie shen jiang'],
   ['宫舞茉歌', '宮舞モカ', 'gong wu mo ge'],
+
+  // # VoiceVox
+  // * 9期生
+  // ['Voidoll', 'Voidoll', ''],
+  ['存子', 'ぞん子', 'cun zi'],
+  ['中部剑', '中部つるぎ', 'zhong bu jian'],
+
+  // * 8期生
+  ['栗田玛珑', '栗田まろん', 'li tian ma long'],
+  ['爱瑠炭', 'あいえるたん', 'ai liu tan'],
+  ['满别花丸', '満別花丸', 'man bie hua wan'],
+  ['琴咏尼娅', '琴詠ニア', 'qin yong ni ya'],
+
+  // * 7期生
+  ['†圣骑士 红樱†', '†聖騎士 紅桜†', 'sheng qi shi hong ying'],
+  ['雀松朱司', '雀松朱司', 'que song zhu si'],
+  ['麒岛宗麟', '麒ヶ島宗麟', 'qi dao zong lin'],
+  ['春歌娜娜', '春歌ナナ', 'chun ge na na'],
+  ['猫使阿鲁', '猫使アル', 'mao shi a lu'],
+  ['猫使碧', '猫使ビィ', 'bao shi bi'],
+  ['中国兔', '中国うさぎ', 'zhong guo tu'],
+
+  // * 6期生
+  // ['', 'ちび式じい', ''],
+  ['樱歌御子', '櫻歌ミコ', 'ying ge yu zi'],
+  ['小夜', '小夜/SAYO', 'xiao ye'],
+  // ['', 'ナースロボ＿タイプＴ', ''],
+
+  // * 5期生
+  ['WhiteCUL', 'WhiteCUL', 'whitecul'],
+  ['后鬼', '後鬼', 'hou gui'],
+  // ['', 'No.7', ''],
+
+  // * 4期生
+  ['望子', 'もち子さん', 'wang zi'],
+  ['剑崎雌雄', '剣崎雌雄', 'jian qi ci xiong'],
+
+  // * 3期生
+  ['玄野武宏', '玄野武宏', 'xuan ye wu hong'],
+  ['白上虎太郎', '白上虎太郎', 'bai shang hu tai lang'],
+  ['青山龙星', '青山龍星', 'qing shan long xing'],
+  ['冥鸣阳葵', '冥鳴ひまり', 'ming ming yang kui'],
+  ['九州空', '九州そら', 'jiu zhou kong'],
+
+  // * 2期生
+  ['春日部紬', '春日部つむぎ', 'chun ri bu chou'],
+  ['雨晴羽雨', '雨晴はう', 'yu qing'],
+  ['波音律', '波音リツ', 'bo yin lv'],
+
+  // * 1期生
+  ['四国玫炭', '四国めたん', 'si guo mei tan'],
+  ['俊达萌', 'ずんだもん', 'jun da meng'],
 ]);
